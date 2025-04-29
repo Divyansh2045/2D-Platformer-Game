ef73b4fed8262714280755a4a0ecba5b6acc1a81
Divyansh <divyanshnagar@hotmail.com>
2025-04-30 02:58:18 +0530

    Crouch animation is executed when control is pressed

--- Game2DPlatformerOutscal/Assets/Scripts/PlayerController.cs
+++ Game2DPlatformerOutscal/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    private BoxCollider2D boxCollider;
+    private Vector2 originalSize;
+    private Vector2 originalOffSet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        boxCollider = GetComponent<BoxCollider2D>();
+        originalSize = boxCollider.size;
+        originalOffSet = boxCollider.offset;
     }
 
     // Update is called once per frame
@@ -27,5 +33,19 @@ public class PlayerController : MonoBehaviour
 
         transform.localScale = scale;
 
+        bool isCrouching = Input.GetKey(KeyCode.LeftControl);
+        animator.SetBool("Crouch", isCrouching);
+
+        if (isCrouching)
+        {
+            boxCollider.size = new Vector2(originalSize.x, originalSize.y / 2f);
+            boxCollider.offset = new Vector2(originalOffSet.x, originalOffSet.y - 0.25f);
+        }
+        else
+        {
+            boxCollider.size = originalSize;
+            boxCollider.offset = originalOffSet;
+        }
+        Debug.Log("Collider size: " + boxCollider.size);
     }
 }
